1b4c262ccb83f54f52c6fed9284e8b01f86dfc50
xlpaladinolx <54073138+xlpaladinolx@users.noreply.github.com>
2019-10-07 10:14:24 -0400

    Ponto Extra referente ao exercício (03/10)
    Ponto Extra referente ao exercício passando em sala de Aula no dia (03/10), código do elevador através de interrupção externa.

---
+++ Elevador.INT.EXT.ino
@@ -0,0 +1,41 @@
+
+#include <Servo.h>
+
+const int pinTerreo = 3;
+const int pin1Andar = 4;
+const int pin2Andar = 5;
+
+const int valTerreo = 0;
+const int valAndar1 = 90;
+const int valAndar2 = 180;
+Servo servo_9;
+
+void Terreo() {
+ 
+  servo_9.write(valTerreo);
+}
+void Andar1() {
+ 
+  servo_9.write(valAndar1);
+}
+void Andar2() {
+
+  servo_9.write(valAndar2);
+}
+
+void setup()
+{
+  servo_9.attach(9);
+  pinMode(pinTerreo, INPUT_PULLUP);
+  pinMode(pin1Andar, INPUT_PULLUP);
+  pinMode(pin2Andar, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pinTerreo), Terreo, RISING);
+  attachInterrupt(digitalPinToInterrupt(pin1Andar), Andar1, RISING);
+  attachInterrupt(digitalPinToInterrupt(pin2Andar), Andar2, RISING);
+  servo_9.write(Terreo);
+}
+
+void loop()
+{
+
+}
